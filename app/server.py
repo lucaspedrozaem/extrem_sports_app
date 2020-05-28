674328947955fea9b351f23005c0602fb5aecce5
lucaspedrozaem <56172594+lucaspedrozaem@users.noreply.github.com>
2020-05-28 16:44:47 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1DZMIwP49TDZR5RnJyrMX5H50aa132s_A'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['base_jumping', 'climbing', 'bungee_jumping', 'wingsuit', 'cliff_jumping', 'ski_jumping', 'regular_jumping']]
 path = Path(__file__).parent
 
 app = Starlette()
