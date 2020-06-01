1519e3a7605dd6955ea6739909797c18b961a2b0
lucaspedrozaem <56172594+lucaspedrozaem@users.noreply.github.com>
2020-06-01 14:42:26 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -30,6 +30,7 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       console.log(response);
+      console.log(response["prob"]);
       el("result-label").innerHTML = `Result = ${response["result"]} `;
       el("probability-label").innerHTML = `Probability = ${response["prob"]} `;
       
