0cd15eaaa0d40dd88eab3b266e010a534a39a795
lucaspedrozaem <56172594+lucaspedrozaem@users.noreply.github.com>
2020-06-01 11:39:46 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,7 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)
-    proba =  str(prediction[2]).replace("tensor(", "").replace(")", "").replace(" ", "").split(",")
+    proba =  str(prediction[2]).replace("tensor([", "").replace("])", "").replace(" ", "").split(",")
     proba_final = []
     for p in proba:
         proba_final.append(float(p))
